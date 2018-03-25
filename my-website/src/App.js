7c9da3568ed5fb16461cd8471970aa7771f230dd
Shane Schmaltz <stschamltz@gmail.com>
2018-03-24 23:11:29 -0600

    re-arrange layout

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -6,13 +6,13 @@ class App extends Component {
     return (
       <div className="landing-body">
         <header className="landing-header">
-          <div className="header-nav-container-inner">
+          <div className="header-nav-container-outer">
             <nav className="header-nav-bar">
               <div className="header-nav-container-inner">
-                <a href="http://localhost:3000/public/" className="header-nav-container__home">Home</a>
-                <a href="" className="header-nav-container__about">About</a>
-                <a href="" className="header-nav-container__resume">Resume</a>
-                <a href="" className="header-nav-container__contact">Contact Me</a>
+                <a href="http://localhost:3000/" className="header-nav-container__home">Home</a>
+                <a href="http://localhost:3000/" className="header-nav-container__about">About</a>
+                <a href="http://localhost:3000/" className="header-nav-container__resume">Resume</a>
+                <a href="http://localhost:3000/" className="header-nav-container__contact">Contact Me</a>
               </div>
             </nav>
           </div>
