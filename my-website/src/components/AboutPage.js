13ac7888636a3da8db3a7bf21e6f17c15bb5de31
Shane Schmaltz <stschamltz@gmail.com>
2018-05-16 19:46:54 -0600

    first version of site

--- my-website/src/components/AboutPage.js
+++ my-website/src/components/AboutPage.js
@@ -16,7 +16,7 @@ class AboutPage extends Component {
          </div>
          <div className="about-3">
             <div className="about-paragraph">
-               <span className="about-paragraph__first">Hi my name is Shane Schmaltz. I’m a software developer from Calgary Alberta Canada. Welcome to my website! I made it myself using react. I have a lot of interests, however I’m most fascinated with software and its ability to change the world. I like to think of myself as a creative dreamer with an knack for problem solving. I also play bass guitar, love to go longboarding, enjoy weightlifting, playing video games and hugely value self improvement and well-being. Thanks for stopping by, please feel free to contact me using any of the links in my contact page.</span>
+               <span className="about-paragraph__first">Hi my name is Shane Schmaltz. I’m a software developer from Calgary Alberta Canada. Welcome to my website! I made it myself using react. I created it so I could share a little bit of myself and what I do with you. I have a lot of interests, however I’m most fascinated with software and its ability to change the world. I like to think of myself as a creative dreamer with an knack for problem solving. I also play bass guitar, love to go longboarding, enjoy weightlifting, playing video games and hugely value self improvement and well-being. Thanks for stopping by, please feel free to contact me using any of the links in my contact page.</span>
                <span className="about-paragraph__second"></span>
             </div>
          </div>
--- my-website/src/components/ContactPage.js
+++ my-website/src/components/ContactPage.js
@@ -5,9 +5,49 @@ class ContactPage extends Component {
   render() {
     return (
       <div className="contact">
-        <h2>This site is made in react by me. It is currently in progress and this is a stub.</h2>
-        <h2>See you in the future!</h2>
-        <h2>Contact Info</h2>
+        <div className="contact-container">
+            <div className="contact-title">
+              <span className="contact-container-title__header">Contact Information:</span>
+            </div>
+            <div className="contact-body">
+              <div className="contact-body__linkedin">
+                <a className="contact-body__linkedin-icon" href="https://www.linkedin.com/in/stschmaltz/">
+                  <i className="fab fa-linkedin" />
+                </a>
+                <span className="contact-body__linkedin-text">
+                  https://www.linkedin.com/in/stschmaltz/
+                </span>
+              </div>
+
+              <div className="contact-body__github">
+                <a className="contact-body__github-icon" href="https://github.com/stschmaltz">
+                  <i className="fab fa-github-square" />
+                </a>
+                <span className="contact-body__github-text">
+                  https://github.com/stschmaltz
+                </span>
+              </div>
+
+              <div className="contact-body__email">
+                <a className="contact-body__email-icon" href="mailto:stschmaltz@gmail.com">
+                  <i className="fas fa-envelope-square" />
+                </a>
+                <span className="contact-body__github-text">
+                  stschmaltz@gmail.com
+                </span>
+              </div>
+
+              <div className="contact-body__instagram">
+                <a className="contact-body__instagram-icon" href="https://instagram.com/wil_shane">
+                  <i className="fab fa-instagram" />
+                </a>
+                <span className="contact-body__instagram-text">
+                  https://instagram.com/wil_shane
+                </span>
+              </div>
+
+            </div>
+        </div>
       </div>
     )
   }
