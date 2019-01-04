5dbfff4004a2ce63d8630f855ee1e6d81ced8d26
Shane Schmaltz <stschmaltz@gmail.com>
2019-01-03 18:52:17 -0700

    tweaks to get my website deployed

--- my-website/src/components/AboutPage.js
+++ my-website/src/components/AboutPage.js
@@ -1,28 +1,36 @@
-import React, {Component} from "react";
-import "../css/styles.css";
+import React, { Component } from 'react';
+import '../css/styles.css';
+import selfImage from '../images/20181225_155058.jpg';
 
 class AboutPage extends Component {
   render() {
     return (
       <div className="about">
-         <div className="about-2">
-            <div className="about-pic">
-               <img src="https://i.imgur.com/81qyN1y.jpg" alt="" className="about-pic__img"/>
-            </div>
-            <div className="about-title">
-               <span className="about-title__intro">Hey Im Shane!</span>
-               <span className="about-title__desc">This is a bit about me.</span>
-            </div>
-         </div>
-         <div className="about-3">
-            <div className="about-paragraph">
-               <span className="about-paragraph__first">Hi my name is Shane Schmaltz. I’m a software developer from Calgary Alberta Canada. Welcome to my website! I made it myself using react. I created it so I could share a little bit of myself and what I do with you. I have a lot of interests, however I’m most fascinated with software and its ability to change the world. I like to think of myself as a creative dreamer with an knack for problem solving. I also play bass guitar, love to go longboarding, enjoy weightlifting, playing video games and hugely value self improvement and well-being. Thanks for stopping by, please feel free to contact me using any of the links in my contact page.</span>
-               <span className="about-paragraph__second"></span>
-            </div>
-         </div>
+        <div className="about-2">
+          <div className="about-pic">
+            <img src={selfImage} alt="" className="about-pic__img" />
+          </div>
+          <div className="about-title">
+            <span className="about-title__intro">Hey Im Shane!</span>
+            <span className="about-title__desc">This is a bit about me.</span>
+          </div>
+        </div>
+        <div className="about-3">
+          <div className="about-paragraph">
+            <span className="about-paragraph__first">
+              Hi my name is Shane Schmaltz. I’m a software developer from
+              Calgary Alberta Canada. Welcome to my website! It was the first
+              site I made with React.. I am currently re-writing it since I have
+              learned tons since I first created it and it no longer showcases
+              my current abilities as a Web Developer. Thanks for stopping by,
+              please feel free to contact me using the contact page.
+            </span>
+            <span className="about-paragraph__second" />
+          </div>
+        </div>
       </div>
-    )
+    );
   }
-};
+}
 
 export default AboutPage;
--- my-website/src/components/ContactPage.js
+++ my-website/src/components/ContactPage.js
@@ -1,56 +1,57 @@
-import React, { Component } from "react";
-import "../css/styles.css";
+import React, { Component } from 'react';
+import '../css/styles.css';
 
 class ContactPage extends Component {
   render() {
     return (
       <div className="contact">
         <div className="contact-container">
-            <div className="contact-title">
-              <span className="contact-container-title__header">Contact Information:</span>
+          <div className="contact-title">
+            <span className="contact-container-title__header">
+              Contact Information:
+            </span>
+          </div>
+          <div className="contact-body">
+            <div className="contact-body__linkedin">
+              <a
+                className="contact-body__linkedin-icon"
+                href="https://www.linkedin.com/in/stschmaltz/"
+              >
+                <i className="fab fa-linkedin" />
+              </a>
+              <span className="contact-body__linkedin-text">
+                https://www.linkedin.com/in/stschmaltz/
+              </span>
             </div>
-            <div className="contact-body">
-              <div className="contact-body__linkedin">
-                <a className="contact-body__linkedin-icon" href="https://www.linkedin.com/in/stschmaltz/">
-                  <i className="fab fa-linkedin" />
-                </a>
-                <span className="contact-body__linkedin-text">
-                  https://www.linkedin.com/in/stschmaltz/
-                </span>
-              </div>
 
-              <div className="contact-body__github">
-                <a className="contact-body__github-icon" href="https://github.com/stschmaltz">
-                  <i className="fab fa-github-square" />
-                </a>
-                <span className="contact-body__github-text">
-                  https://github.com/stschmaltz
-                </span>
-              </div>
-
-              <div className="contact-body__email">
-                <a className="contact-body__email-icon" href="mailto:stschmaltz@gmail.com">
-                  <i className="fas fa-envelope-square" />
-                </a>
-                <span className="contact-body__github-text">
-                  stschmaltz@gmail.com
-                </span>
-              </div>
-
-              <div className="contact-body__instagram">
-                <a className="contact-body__instagram-icon" href="https://instagram.com/wil_shane">
-                  <i className="fab fa-instagram" />
-                </a>
-                <span className="contact-body__instagram-text">
-                  https://instagram.com/wil_shane
-                </span>
-              </div>
-
+            <div className="contact-body__github">
+              <a
+                className="contact-body__github-icon"
+                href="https://github.com/stschmaltz"
+              >
+                <i className="fab fa-github-square" />
+              </a>
+              <span className="contact-body__github-text">
+                https://github.com/stschmaltz
+              </span>
             </div>
+
+            <div className="contact-body__email">
+              <a
+                className="contact-body__email-icon"
+                href="mailto:stschmaltz@gmail.com"
+              >
+                <i className="fas fa-envelope-square" />
+              </a>
+              <span className="contact-body__github-text">
+                stschmaltz@gmail.com
+              </span>
+            </div>
+          </div>
         </div>
       </div>
-    )
+    );
   }
-};
+}
 
 export default ContactPage;
